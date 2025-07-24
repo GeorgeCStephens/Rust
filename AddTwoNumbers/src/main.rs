b54a3b1bb893095af039a9e319b75d204b724b2d
GeorgeCStephens <dga2601@gmail.com>
2025-07-24 11:07:45 +0100

    Removed input on AddingTwoNumbers

--- AddTwoNumbers/src/main.rs
+++ AddTwoNumbers/src/main.rs
@@ -1,8 +1,4 @@
 fn main() {
-    println!("Enter your name :");
-    let b1 = std::io::stdin();
-    println!("Hello {}", b1);
-    
     let num1: f64 = 5.46;
     let num2: f64 = 3.57;
     let sum = num1 + num2;
